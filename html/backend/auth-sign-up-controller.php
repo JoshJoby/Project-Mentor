315a466b0fe298cf35be015fc3c7bb7b50071625
Josh Joby <85182505+JoshJoby@users.noreply.github.com>
2023-06-29 19:19:16 +0530

    first commit from vs

---
+++ html/backend/auth-sign-up-controller.php
@@ -0,0 +1,59 @@
+<?php
+// Database credentials
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "gridproject1";
+
+// Create a connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check the connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$fname = $_POST['fname']; // Assuming the email input field has the name attribute set to "email"
+echo $fname;
+$lname = $_POST['lname']; // Assuming the email input field has the name attribute set to "email"
+echo $lname;
+$email = $_POST['email']; // Assuming the email input field has the name attribute set to "email"
+echo $email;
+$phno = $_POST['phno']; // Assuming the email input field has the name attribute set to "email"
+echo $phno;
+$password = $_POST['password'];
+echo $password;
+$confpassword = $_POST['confpassword'];
+echo $confpassword;
+$sql = "SELECT * FROM users WHERE email='$email'";
+
+// Execute the query
+$result = $conn->query($sql);
+
+if ($result->num_rows == 0) {
+
+    if ($password === $confpassword) {
+        $randomNumber = rand(10000, 99999);
+        echo $randomNumber;
+        $sql = "INSERT INTO users (user_id, first_name, last_name, email, password, phone_number) VALUES ($randomNumber, '$fname', '$lname', '$email', '$password', '$phno')";
+
+        if (mysqli_query($conn, $sql)) {
+            echo "Data inserted successfully.";
+            echo "User authentication successful!";
+            header("Location: auth-sign-in.php");
+            exit();
+        } else {
+            echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+        }
+    } else {
+        // Password is incorrect
+        echo "A User with the entered email already exists!";
+    }
+} else {
+    // User does not exist
+    echo "User not found!";
+}
+
+// Close the connection
+$conn->close();
+?>
---
+++ html/backend/auth-sign-up.php
@@ -0,0 +1,181 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+   <meta charset="utf-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+   <title>Webkit | Responsive Bootstrap 4 Admin Dashboard Template</title>
+
+   <!-- Favicon -->
+   <link rel="shortcut icon" href="../assets/images/favicon.ico" />
+   <link rel="stylesheet" href="../assets/css/backend-plugin.min.css">
+   <link rel="stylesheet" href="../assets/css/backend.css?v=1.0.0">
+   <link rel="stylesheet" href="../assets/vendor/line-awesome/dist/line-awesome/css/line-awesome.min.css">
+   <link rel="stylesheet" href="../assets/vendor/remixicon/fonts/remixicon.css">
+
+   <link rel="stylesheet" href="../assets/vendor/tui-calendar/tui-calendar/dist/tui-calendar.css">
+   <link rel="stylesheet" href="../assets/vendor/tui-calendar/tui-date-picker/dist/tui-date-picker.css">
+   <link rel="stylesheet" href="../assets/vendor/tui-calendar/tui-time-picker/dist/tui-time-picker.css">
+
+   <style>
+      .auth-card {
+         opacity: 0;
+         animation-name: fade-in;
+         animation-duration: 0.5s;
+         animation-delay: 0.5s;
+         animation-fill-mode: forwards;
+         border: none;
+         height: fit-content;
+         box-sizing: border-box;
+         margin-bottom: -10px;
+         /* Adjust the value as needed */
+      }
+      
+
+      @keyframes fade-in {
+         from {
+            opacity: 0;
+         }
+
+         to {
+            opacity: 1;
+         }
+      }
+   </style>
+</head>
+
+<body class=" "
+   style="background-image: url('../assets/images/login/login_overlay.jpg'); background-size: cover; background-position: center;">
+   <!-- loader Start -->
+   <div id="loading">
+      <div id="loading-center">
+      </div>
+   </div>
+   <!-- loader END -->
+
+   <div class="wrapper">
+      <section class="login-content">
+         <div class="overlay"
+            style="position: absolute; top: 0; left: 0; width: 100%; height: 100%; background-color: rgba(0, 0, 0, 0.5); z-index: -1;">
+         </div>
+         <div class="container">
+            <div class="row align-items-center justify-content-center height-self-center">
+               <div class="col-lg-8">
+                  <div class="card auth-card">
+                     <div class="card-body p-0">
+                        <div class="d-flex align-items-center auth-content">
+                           <div class="col-lg-6 bg-primary content-left">
+                              <div class="p-3">
+                                 <h2 class="mb-2 text-white">Sign Up</h2>
+                                 <p>Create your Webkit account.</p>
+                                 <form action="auth-sign-up-controller.php" method="post">
+                                    <div class="row">
+                                       <div class="col-lg-6">
+                                          <div class="floating-label form-group">
+                                             <input class="floating-input form-control" type="text" placeholder=" "
+                                                name="fname">
+                                             <label>First Name</label>
+                                          </div>
+                                       </div>
+                                       <div class="col-lg-6">
+                                          <div class="floating-label form-group">
+                                             <input class="floating-input form-control" type="text" placeholder=" "
+                                                name="lname">
+                                             <label>Last Name</label>
+                                          </div>
+                                       </div>
+                                       <div class="col-lg-6">
+                                          <div class="floating-label form-group">
+                                             <input class="floating-input form-control" type="email" placeholder=" "
+                                                name="email">
+                                             <label>Email</label>
+                                          </div>
+                                       </div>
+                                       <div class="col-lg-6">
+                                          <div class="floating-label form-group">
+                                             <input class="floating-input form-control" type="text" placeholder=" "
+                                                name="phno">
+                                             <label>Phone No.</label>
+                                          </div>
+                                       </div>
+                                       <div class="col-lg-6">
+                                          <div class="floating-label form-group">
+                                             <input class="floating-input form-control" type="password" placeholder=" "
+                                                name="password">
+                                             <label>Password</label>
+                                          </div>
+                                       </div>
+                                       <div class="col-lg-6">
+                                          <div class="floating-label form-group">
+                                             <input class="floating-input form-control" type="password" placeholder=" "
+                                                name="confpassword">
+                                             <label>Confirm Password</label>
+                                          </div>
+                                       </div>
+                                       <div class="col-lg-12">
+                                          <div class="custom-control custom-checkbox mb-3">
+                                             <input type="checkbox" class="custom-control-input" id="customCheck1">
+                                             <label class="custom-control-label text-white" for="customCheck1">I agree
+                                                with the terms of use</label>
+                                          </div>
+                                       </div>
+                                    </div>
+                                    <button type="submit" class="btn btn-white" id="signupButton" disabled>Sign
+                                       Up</button>
+                                    <p class="mt-3">
+                                       Already have an Account <a href="auth-sign-in.php"
+                                          class="text-white text-underline">Sign In</a>
+                                    </p>
+                                 </form>
+                              </div>
+                           </div>
+                           <div class="col-lg-6 content-right">
+                              <img src="../assets/images/login/01.png" class="img-fluid image-right" alt="">
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </section>
+   </div>
+
+   <script>
+      var checkbox = document.getElementById('customCheck1');
+      var signupButton = document.getElementById('signupButton');
+      var authCard = document.querySelector('.auth-card');
+      var originalHeight = authCard.offsetHeight;
+
+      checkbox.addEventListener('change', function () {
+         if (checkbox.checked) {
+            signupButton.disabled = false;
+         } else {
+            signupButton.disabled = true;
+         }
+      });
+   </script>
+
+
+
+   <!-- Backend Bundle JavaScript -->
+   <script src="../assets/js/backend-bundle.min.js"></script>
+
+   <!-- Table Treeview JavaScript -->
+   <script src="../assets/js/table-treeview.js"></script>
+
+   <!-- Chart Custom JavaScript -->
+   <script src="../assets/js/customizer.js"></script>
+
+   <!-- Chart Custom JavaScript -->
+   <script async src="../assets/js/chart-custom.js"></script>
+   <!-- Chart Custom JavaScript -->
+   <script async src="../assets/js/slider.js"></script>
+
+   <!-- app JavaScript -->
+   <script src="../assets/js/app.js"></script>
+
+   <script src="../assets/vendor/moment.min.js"></script>
+</body>
+
+</html>
