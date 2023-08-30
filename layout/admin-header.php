6a2ea168cf7564b5e6066e073e75cf66a188868b
itachi0501 <aryankhatri02@gmail.com>
2023-08-30 17:43:48 +0530

    changes made again wrt rerouting

--- layout/admin-header.php
+++ layout/admin-header.php
@@ -34,25 +34,25 @@
                     </a>
                 </li>
                 <li class="">
-                    <a href="/ProjectMentor/admin/page-task" class="svg-icon">
+                    <a href="/ProjectMentor/admin/page-employee" class="svg-icon">
                         <svg class="svg-icon" width="25" height="25" xmlns="http://www.w3.org/2000/svg"
                             viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
                             stroke-linecap="round" stroke-linejoin="round">
                             <path d="M16 4h2a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h2"></path>
                             <rect x="8" y="2" width="8" height="4" rx="1" ry="1"></rect>
                         </svg>
-                        <span class="ml-4">Add Task</span>
+                        <span class="ml-4">Add Projects</span>
                     </a>
                 </li>
                 <li class=" ">
-                    <a href="/ProjectMentor/admin/page-employee" class="svg-icon">
+                    <a href="/ProjectMentor/admin/page-task" class="svg-icon">
                     <svg class="svg-icon" width="25" height="25" xmlns="http://www.w3.org/2000/svg"
                             viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
                             stroke-linecap="round" stroke-linejoin="round">
                             <path d="M16 4h2a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h2"></path>
                             <rect x="8" y="2" width="8" height="4" rx="1" ry="1"></rect>
                         </svg>
-                        <span class="ml-4">Add Project</span>
+                        <span class="ml-4">Add tasks</span>
                     </a>
                 </li>
                 <!-- <li class="">
