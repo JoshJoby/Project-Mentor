e65c72f73fcd3833743b3bd251552fa2a87a2e0e
kavitha002 <kavitha02aradhya@gmail.com>
2023-09-03 00:10:55 +0530

    project_controller added

---
+++ student/project-controller.php
@@ -0,0 +1,45 @@
+<?php
+// Database credentials
+// $servername = "localhost";
+// $username = "root";
+// $password = "";
+// $dbname = "projectmentor";
+// date_default_timezone_set('Asia/Kolkata');
+
+// // Create a connection
+// $conn = new mysqli($servername, $username, $password, $dbname);
+
+// // Check the connection
+// if ($conn->connect_error) {
+//     die("Connection failed: " . $conn->connect_error);
+// }
+include '../config_local.php';
+
+// $currentDate = date("Y-m-d");
+// $current_time = date("H:i:s");
+
+$sql = "SELECT *
+FROM completed_projects";
+
+// echo $sql;   
+// Execute the query
+$result = $conn->query($sql);
+
+// Initialize an array to store rows
+$rows = array();
+
+if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        $rows[] = $row;
+    }
+    // Store the rows array in a session variable
+    // session_start();
+    $_SESSION['completed_project_rows'] = $rows;
+}
+else{
+    unset($_SESSION['completed_project_rows']);
+}
+
+// Close the connection
+$conn->close();
+?>
