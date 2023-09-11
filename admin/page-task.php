c1be9598f50f61b5adccc846567e91438e0eb618
Josh Joby <85182505+JoshJoby@users.noreply.github.com>
2023-09-11 22:56:32 +0530

    in process of updating tasks module for amdin

--- admin/page-task.php
+++ admin/page-task.php
@@ -124,14 +124,14 @@ include 'add-task.php';
                                                             <div class='form-group mb-0'>
                                                                 <label for='exampleInputText2' class='h5'>Task Name</label>
                                                                 <div class='form-group mb-3 position-relative'>
-                                                                <input type='text' class='form-control bg-white' placeholder='<Load Task Name from DB>'>
+                                                                <input type='text' class='form-control bg-white' placeholder='" . $row['task_name'] . "'>
                                                             </div>
                                                             </div>
                                                         </div>
                                                         <div class='col-lg-6'>
                                                             <div class='form-group mb-0'>
                                                                 <label for='exampleInputText3' class='h5'>Task Duration</label>
-                                                                <input type='text' class='form-control bg-white' placeholder='<Load Task Duration from DB>'>
+                                                                <input type='text' class='form-control bg-white' placeholder='" . $row['task_duration'] . "'>
                                                             </div>                        
                                                         </div>
                                                     </div>
@@ -142,7 +142,7 @@ include 'add-task.php';
                                                     <div class='row'>
                                                         <div class='col-lg-12'>                                                        
                                                             <h5 class='mb-2'>Description</h5>
-                                                            <p class='mb-0'>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet. [Load from DB]</p>
+                                                            <p class='mb-0'>" . $row['task_desc'] . "</p>
                                                         </div>
                                                         
                                                     </div>
@@ -174,56 +174,8 @@ include 'add-task.php';
         <!-- Wrapper End-->
 
         <!-- Modal list start -->
-        <div class='modal fade' role='dialog' aria-modal='true' id='new-project-modal'>
-            <div class='modal-dialog  modal-dialog-centered' role='document'>
-                <div class='modal-content'>
-                    <div class='modal-header d-block text-center pb-3 border-bttom'>
-                        <h3 class='modal-title' id='exampleModalCenterTitle01'>New Project</h3>
-                    </div>
-                    <div class='modal-body'>
-                        <div class='row'>
-                            <div class='col-lg-12'>
-                                <div class='form-group mb-3'>
-                                    <label for='exampleInputText01' class='h5'>Project Name*</label>
-                                    <input type='text' class='form-control' id='exampleInputText01'
-                                        placeholder='Project Name'>
-                                </div>
-                            </div>
-                            <div class='col-lg-6'>
-                                <div class='form-group mb-3'>
-                                    <label for='exampleInputText2' class='h5'>Categories *</label>
-                                    <select name='type' class='selectpicker form-control' data-style='py-0'>
-                                        <option>Category</option>
-                                        <option>Android</option>
-                                        <option>IOS</option>
-                                        <option>Ui/Ux Design</option>
-                                        <option>Development</option>
-                                    </select>
-                                </div>
-                            </div>
-                            <div class='col-lg-6'>
-                                <div class='form-group mb-3'>
-                                    <label for='exampleInputText004' class='h5'>Due Dates*</label>
-                                    <input type='date' class='form-control' id='exampleInputText004' value=''>
-                                </div>
-                            </div>
-                            <div class='col-lg-12'>
-                                <div class='form-group mb-3'>
-                                    <label for='exampleInputText07' class='h5'>Assign Members*</label>
-                                    <input type='text' class='form-control' id='exampleInputText07'>
-                                </div>
-                            </div>
-                            <div class='col-lg-12'>
-                                <div class='d-flex flex-wrap align-items-ceter justify-content-center mt-2'>
-                                    <div class='btn btn-primary mr-3' data-dismiss='modal'>Save</div>
-                                    <div class='btn btn-primary' data-dismiss='modal'>Cancel</div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+        
+        
         <div class='modal fade bd-example-modal-lg' role='dialog' aria-modal='true' id='new-task-modal'>
             <div class='modal-dialog  modal-dialog-centered modal-lg' role='document'>
                 <div class='modal-content'>
@@ -231,43 +183,33 @@ include 'add-task.php';
                         <h3 class='modal-title' id='exampleModalCenterTitle'>New Task</h3>
                     </div>
                     <div class='modal-body'>
-                        <form action="add-task.php" method="post" enctype="multipart/form-data">
+                        <form action="add-task.php" method="POST" enctype="multipart/form-data">
                             <div class='row'>
                                 <div class='col-lg-12'>
                                     <div class='form-group mb-3'>
                                         <label for='exampleInputText02' class='h5'>Task Name</label>
                                         <input type='text' class='form-control' id='exampleInputText02' name='task_name'
                                             placeholder='Enter task Name'>
-                                        <a href='#' class='task-edit text-body'><i class='ri-edit-box-line'></i></a>
                                     </div>
                                 </div>
-                                <div class='col-lg-4'>
+                                <div class='col-lg-6'>
                                     <div class='form-group mb-3'>
-                                        <label for='exampleInputText2' class='h5'>Assigned to</label>
-                                        <select name='assigned_to' class='selectpicker form-control' data-style='py-0'>
-                                            <option>Memebers</option>
-                                            <option>Kianna Septimus</option>
-                                            <option>Jaxson Herwitz</option>
-                                            <option>Ryan Schleifer</option>
-                                        </select>
+                                        <label for='exampleInputText05' class='h5'>Task Duration</label>
+                                        <input type='text' class='form-control' id='exampleInputText03' name='task_duration'
+                                            placeholder='Enter task Duration'>
                                     </div>
                                 </div>
-                                <div class='col-lg-4'>
+                                <div class='col-lg-6'>
                                     <div class='form-group mb-3'>
-                                        <label for='exampleInputText05' class='h5'>Due Dates*</label>
-                                        <input type='date' name='due_date' class='form-control' id='exampleInputText05'
-                                            value=''>
-                                    </div>
-                                </div>
-                                <div class='col-lg-4'>
-                                    <div class='form-group mb-3'>
-                                        <label for='exampleInputText2' class='h5'>Category</label>
+                                        <label for='exampleInputText2' class='h5'>Skills Required</label>
                                         <select name='category' class='selectpicker form-control' data-style='py-0'>
-                                            <option>Design</option>
-                                            <option>Android</option>
-                                            <option>IOS</option>
+                                            <option>Front-End Development</option>
+                                            <option>Back-End Development</option>
+                                            <option>Full Stack Development</option>
                                             <option>Ui/Ux Design</option>
-                                            <option>Development</option>
+                                            <option>Mobile App Development</option>
+                                            <option>Artificial Intelligence</option>
+                                            <option>Machine Learning</option>   
                                         </select>
                                     </div>
                                 </div>
@@ -277,7 +219,7 @@ include 'add-task.php';
                                         <textarea class='form-control' name='description' id='exampleInputText040'
                                             rows='2'></textarea>
                                     </div>
-                                </div>
+                                </div>                                                          
                                 <!-- <div class='col-lg-12'>
                                 <div class='form-group mb-3'>
                                     <label for='exampleInputText005' class='h5'>Checklist</label>
@@ -290,7 +232,7 @@ include 'add-task.php';
                                         <label for='exampleInputText01' class='h5'>Attachments</label>
                                         <div class='custom-file'>
                                             <input type='file' class='custom-file-input' name='attached'
-                                                id='inputGroupFile003'>
+                                                id='inputGroupFile003'>                                                                                             
                                             <label class='custom-file-label' for='inputGroupFile003'>Upload
                                                 media</label>
                                         </div>
